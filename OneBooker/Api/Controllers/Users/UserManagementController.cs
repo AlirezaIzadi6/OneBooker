313dcdd8a0acd5eab2d05b558e70587a8cb4ef19
Alireza Izadi <alirezaizadi199912@gmail.com>
2026-05-06 23:26:59 +0330

    doc: complete documentation for user management controller

--- OneBooker/Api/Controllers/Users/UserManagementController.cs
+++ OneBooker/Api/Controllers/Users/UserManagementController.cs
@@ -7,28 +7,35 @@ using OneBooker.Shared.Responses.ServiceResponse;
 namespace OneBooker.Api.Controllers.Users;
 
 /// <summary>
-/// 
+/// Manage system users.
 /// </summary>
-/// <param name="registrationService"></param>
-/// <param name="loginService"></param>
 [ApiController]
-[Route("api/{versioning:apiVersion}/[controller]")]
+[Route("api/v{versioning:apiVersion}/[controller]")]
 [ApiVersion("1.0")]
 public class UserManagementController(IUserRegistrationService registrationService, IUserLoginService loginService) : ControllerBase
 {
+    /// <summary>
+    /// Register a new user.
+    /// </summary>
+    /// <param name="request"><see cref="RegistrationRequest"/> containing new user info.</param>
+    /// <returns>Unique identifier of the new user if created successfully.</returns>
+    /// <response code="200">New user created successfully.</response>
+    /// <response code="400">Provided information are not valid.</response>
     [HttpPost("register")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<Response<int>>> RegisterUser([FromBody] RegistrationRequest request)
     {
         return await registrationService.RegisterUser(request);
     }
 
     /// <summary>
-    /// Login with this endpoint.
+    /// Login with user credentials.
     /// </summary>
     /// <param name="request"><see cref="LoginRequest"/> object containing user's credentials.</param>
     /// <returns>Access token if login is successful, error message otherwise.</returns>
     /// <response code="200">Login was successful.</response>
-    /// <response code="400">Login credentials were incorrect.</response>
+    /// <response code="401">Login credentials were incorrect.</response>
     [HttpPost("login")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(JwtResponse))]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
